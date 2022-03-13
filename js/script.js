393ca3cef1dcf060332866d5d298f73d82f8fdc9
Pedro <pedrojuniorifpb@gmail.com>
2022-03-12 21:13:14 -0300

    Versão funcional com bugs

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let resultado = document.querySelector("#resultado");
+let vetorCalc = [];
 
 document.querySelector("#button_01").addEventListener("click", () => buttonNumber("1"));
 document.querySelector("#button_02").addEventListener("click", () => buttonNumber("2"));
@@ -12,20 +13,59 @@ document.querySelector("#button_07").addEventListener("click", () => buttonNumbe
 document.querySelector("#button_08").addEventListener("click", () => buttonNumber("8"));
 document.querySelector("#button_09").addEventListener("click", () => buttonNumber("9"));
 
-document.querySelector("#ponto").addEventListener("click", () => adicionarOperacao("."));
+document.querySelector("#ponto").addEventListener("click", () => adicionarOperadorOuPonto("."));
 document.querySelector("#zero").addEventListener("click", () => buttonNumber("0"));
-
 document.querySelector("#limpar").addEventListener("click", () => limpar());
 
-document.querySelector("#somar").addEventListener("click", () => adicionarOperacao("+"));
-document.querySelector("#subtrair").addEventListener("click", () => adicionarOperacao("-"));
-document.querySelector("#multiplicar").addEventListener("click", () => adicionarOperacao("x"));
-document.querySelector("#dividir").addEventListener("click", () => adicionarOperacao("/"));
+document.querySelector("#somar").addEventListener("click", () => adicionarOperadorOuPonto("+"));
+document.querySelector("#subtrair").addEventListener("click", () => adicionarOperadorOuPonto("-"));
+document.querySelector("#multiplicar").addEventListener("click", () => adicionarOperadorOuPonto("x"));
+document.querySelector("#dividir").addEventListener("click", () => adicionarOperadorOuPonto("/"));
 
 document.querySelector("#button_igual").addEventListener("click", () => {
+
+    let vetor = separarElementosNoVetor(resultado.value);
+    
+    if (vetor.length == 1) {
+        null;
+    } else {
+        vetor = CalcularOperacoesPrioritarias(vetor);
+    }
+
+    let resposta = somarElementosDentroDoVetor(vetor);
+
+    resultado.value = String(resposta);
+
+})
+
+function buttonNumber(buttonPresionado) {
+    resultado.value += buttonPresionado;
+}
+
+function limpar() {
+    resultado.value = "";
+}
+
+function adicionarOperadorOuPonto(operacao) {
+    let indice = resultado.value.length - 1;
+    let ultimoElemento = resultado.value[indice];
+
+    if ((ultimoElemento != "-" && ultimoElemento != ".") && operacao == "-") {
+        resultado.value += operacao;
+    } else if (resultado.value == "") {
+        null;
+    } else if (verificarOperador(ultimoElemento) == false) {
+        if (ultimoElemento == ".") {
+            null;
+        } else {
+            resultado.value += operacao;
+        }
+    }
+}
+
+function separarElementosNoVetor(elementos) {
     let numero = "";
     let vetor = [];
-    let elementos = resultado.value;
 
     for (let cont = 0; cont <= elementos.length; cont++) {
         if (verificarOperador(elementos[cont]) == true) {
@@ -39,51 +79,44 @@ document.querySelector("#button_igual").addEventListener("click", () => {
         }
     }
 
-    // for (let cont = 0; cont)
+    let indice = vetor.length - 1;
+    let ultimoElemento = vetor[indice];
+    let indiceElemento = ultimoElemento.length - 1;
 
-    let cont = 1;
-    let resposta;
-
-    if (vetor[0] == "-") {
-        resposta = Number(vetor[1]) * -1;
-        cont = 2;
-    } else {
-        resposta = Number(vetor[0]);
+    if (ultimoElemento[indiceElemento] == ".") {
+        vetor[indice] = Number(vetor[indice]);
+    } else if (verificarOperador(ultimoElemento) == true) {
+        vetor.splice(vetor[indice]);
     }
 
-    for (; cont < vetor.length; cont += 2) {
-        if (vetor[cont] == "x") {
-            resposta *= Number(vetor[cont + 1]);
-        } else if (vetor[cont] == "/") {
-            resposta /= Number(vetor[cont + 1]);
-        } else if (vetor[cont] == "+") {
-            resposta += Number(vetor[cont + 1])
-        } else if (vetor[cont] == "-") {
-            resposta -= Number(vetor[cont + 1]);
+    return vetor;
+}
+
+function CalcularOperacoesPrioritarias(vetor) {
+    let novaLista = [];
+
+    for (let cont = 0; cont < vetor.length; cont++) {
+        if (vetor[cont] == "x" || vetor[cont] == "/") {
+            let resposta = calcularElementos(vetor[cont - 1], vetor[cont], vetor[cont + 1]);
+            novaLista.push(resposta);
+        } else if ((vetor[cont] == "+" || vetor[cont] == "-")) {
+            let numero = Number(vetor[cont] + vetor[cont + 1]);
+            novaLista.push(numero);
         }
     }
-    resultado.value = String(resposta);
-})
 
-function buttonNumber(buttonPresionado) {
-    resultado.value += buttonPresionado;
+    // Retorna aqui um vetor com apenas inteiros
+    return novaLista;
 }
 
-function limpar() {
-    resultado.value = "";
-}
+function somarElementosDentroDoVetor(vetor) {
+    let resposta = 0;
 
-function adicionarOperacao(operacao) {
-    let indice = resultado.value.length - 1;
-    let ultimoElemento = resultado.value[indice];
-
-    if (ultimoElemento != "-" && operacao == "-") {
-        resultado.value += operacao;
-    } else if (resultado.value == "") {
-        null;
-    } else if (verificarOperador(ultimoElemento) == false) {
-        resultado.value += operacao;
+    for (let numero of vetor) {
+        resposta += numero;
     }
+
+    return resposta;
 }
 
 function verificarOperador(valor) {
@@ -98,5 +131,20 @@ function verificarOperador(valor) {
             return true;
         default:
             return false;
+    }
+}
+
+function calcularElementos(numeroAntes, operador, numeroDepois) {
+    switch (operador) {
+        case "+":
+            return numeroAntes + numeroDepois;
+        case "-":
+            return numeroAntes - numeroDepois;
+        case "x":
+            return numeroAntes * numeroDepois;
+        case "/":
+            return numeroAntes / numeroDepois;
+        default:
+            return 0;
     }
 }
