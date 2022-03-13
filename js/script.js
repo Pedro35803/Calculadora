e641e7b32dc920cd9db8813e9e94f7e9f6ec4ca3
Pedro <pedrojuniorifpb@gmail.com>
2022-03-13 10:42:30 -0300

    Remoderanod o funcionamento da calculadora e do código

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 let resultado = document.querySelector("#resultado");
-let vetorCalc = [];
+let guardarOperador = false;
+let mudarNumero = true;
+let guardarNumero = 0;
 
 document.querySelector("#button_01").addEventListener("click", () => buttonNumber("1"));
 document.querySelector("#button_02").addEventListener("click", () => buttonNumber("2"));
@@ -13,125 +15,64 @@ document.querySelector("#button_07").addEventListener("click", () => buttonNumbe
 document.querySelector("#button_08").addEventListener("click", () => buttonNumber("8"));
 document.querySelector("#button_09").addEventListener("click", () => buttonNumber("9"));
 
-document.querySelector("#ponto").addEventListener("click", () => adicionarOperadorOuPonto("."));
+document.querySelector("#ponto").addEventListener("click", () => adicionarPonto());
 document.querySelector("#zero").addEventListener("click", () => buttonNumber("0"));
 document.querySelector("#limpar").addEventListener("click", () => limpar());
 
-document.querySelector("#somar").addEventListener("click", () => adicionarOperadorOuPonto("+"));
-document.querySelector("#subtrair").addEventListener("click", () => adicionarOperadorOuPonto("-"));
-document.querySelector("#multiplicar").addEventListener("click", () => adicionarOperadorOuPonto("x"));
-document.querySelector("#dividir").addEventListener("click", () => adicionarOperadorOuPonto("/"));
+document.querySelector("#somar").addEventListener("click", () => calcular("+"));
+document.querySelector("#subtrair").addEventListener("click", () => calcular("-"));
+document.querySelector("#multiplicar").addEventListener("click", () => calcular("x"));
+document.querySelector("#dividir").addEventListener("click", () => calcular("/"));
 
 document.querySelector("#button_igual").addEventListener("click", () => {
-
-    let vetor = separarElementosNoVetor(resultado.value);
-    
-    if (vetor.length == 1) {
-        null;
-    } else {
-        vetor = CalcularOperacoesPrioritarias(vetor);
+    if (resultado.value != "") {
+        calcular(false);
+        piscarAoFazerUmaOperação();
     }
-
-    let resposta = somarElementosDentroDoVetor(vetor);
-
-    resultado.value = String(resposta);
-
 })
 
 function buttonNumber(buttonPresionado) {
+    if (mudarNumero == true) {
+        resultado.value = "";
+        mudarNumero = false;
+    }
     resultado.value += buttonPresionado;
 }
 
 function limpar() {
+    guardarOperador = false;
     resultado.value = "";
+    guardarNumero = 0;
 }
 
-function adicionarOperadorOuPonto(operacao) {
-    let indice = resultado.value.length - 1;
-    let ultimoElemento = resultado.value[indice];
-
-    if ((ultimoElemento != "-" && ultimoElemento != ".") && operacao == "-") {
-        resultado.value += operacao;
-    } else if (resultado.value == "") {
-        null;
-    } else if (verificarOperador(ultimoElemento) == false) {
-        if (ultimoElemento == ".") {
-            null;
+function calcular(operador) {
+    if (resultado.value != "" && mudarNumero == false) {
+        let numero = Number(resultado.value);
+    
+        if (guardarOperador != false) {
+            guardarNumero = calcularElementos(guardarNumero, guardarOperador, numero)
         } else {
-            resultado.value += operacao;
+            guardarNumero = numero;
         }
+    
+        guardarOperador = operador;
+        mudarNumero = true;
+
+        piscarAoFazerUmaOperação();
     }
 }
 
-function separarElementosNoVetor(elementos) {
-    let numero = "";
-    let vetor = [];
-
-    for (let cont = 0; cont <= elementos.length; cont++) {
-        if (verificarOperador(elementos[cont]) == true) {
-            vetor.push(numero);
-            vetor.push(elementos[cont]);
-            numero = "";
-        } else if (cont == elementos.length) {
-            vetor.push(numero);
-        } else {
-            numero += elementos[cont];
-        }
+function adicionarPonto() {
+    if (resultado.value.indexOf(".") == -1 && resultado.value != "") {
+        resultado.value += "."
     }
-
-    let indice = vetor.length - 1;
-    let ultimoElemento = vetor[indice];
-    let indiceElemento = ultimoElemento.length - 1;
-
-    if (ultimoElemento[indiceElemento] == ".") {
-        vetor[indice] = Number(vetor[indice]);
-    } else if (verificarOperador(ultimoElemento) == true) {
-        vetor.splice(vetor[indice]);
-    }
-
-    return vetor;
 }
 
-function CalcularOperacoesPrioritarias(vetor) {
-    let novaLista = [];
-
-    for (let cont = 0; cont < vetor.length; cont++) {
-        if (vetor[cont] == "x" || vetor[cont] == "/") {
-            let resposta = calcularElementos(vetor[cont - 1], vetor[cont], vetor[cont + 1]);
-            novaLista.push(resposta);
-        } else if ((vetor[cont] == "+" || vetor[cont] == "-")) {
-            let numero = Number(vetor[cont] + vetor[cont + 1]);
-            novaLista.push(numero);
-        }
-    }
-
-    // Retorna aqui um vetor com apenas inteiros
-    return novaLista;
-}
-
-function somarElementosDentroDoVetor(vetor) {
-    let resposta = 0;
-
-    for (let numero of vetor) {
-        resposta += numero;
-    }
-
-    return resposta;
-}
-
-function verificarOperador(valor) {
-    switch (valor) {
-        case "+":
-            return true;
-        case "-":
-            return true;
-        case "x":
-            return true;
-        case "/":
-            return true;
-        default:
-            return false;
-    }
+function piscarAoFazerUmaOperação() {
+    resultado.value = "";
+    setTimeout(() => {
+        resultado.value = guardarNumero;
+    }, 50);
 }
 
 function calcularElementos(numeroAntes, operador, numeroDepois) {
